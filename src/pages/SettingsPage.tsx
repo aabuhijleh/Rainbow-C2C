d36eb3b58382896efbd75a9cecbe2738c81e9436
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2021-04-27 16:36:56 +0300

    feat: add localization

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Header } from "components/Header";
+import { labels } from "labels";
 
 export const SettingsPage: React.FC = () => {
-  return <Header title="Telephony Settings" toPage="about" />;
+  return <Header title={labels.telephonySettings} toPage="about" />;
 };
